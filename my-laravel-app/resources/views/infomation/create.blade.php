642de1bf3d3341834da76beb3f0a1755e2eae0e1
hyurai <yuumanagai0504@icloud.com>
2021-02-27 15:40:11 +0900

    layout&blade_complete

--- my-laravel-app/resources/views/infomation/create.blade.php
+++ my-laravel-app/resources/views/infomation/create.blade.php
@@ -1,23 +1,4 @@
 @extends('infomation/layout')
 @section('title')
-<div class = "create_contents">
-  <form action= "/infomation" method = "post">
-  @csrf
-  <input type="hidden" name="_method" value="POST">
-    <div class = "height_create">
-      <label for="height">身長</label>
-      <input type="text" name = "height" >
-    </div>
-    <div class = "weight_create">
-      <label for="weight">体重</label>
-      <input type="text" name = "weight">
-    </div>
-    <div>
-      <label for="name_create">名前</label>
-      <input type="text" name = "name">
-    </div>
-    <button type = "submit">登録</button>
-    <a href="/infomation">戻る</a>
-  </form>
-</div>
-@section('title')
+@include('infomation/form',['target' => 'store'])
+@endsection
--- my-laravel-app/resources/views/infomation/edit.blade.php
+++ my-laravel-app/resources/views/infomation/edit.blade.php
@@ -1,23 +1,4 @@
 @extends('infomation/layout')
 @section('title')
-<div class = "edit_contents">
-  <form action = "/infomation/{{$infomation->id}}" method = "POST">
-  @csrf 
-    <input type="hidden" name="_method" value="PUT">
-    <div class = "height_edit">
-      <label for="height">身長</label>
-      <input type="text" name = "height" value = "{{$infomation->height}}">
-    </div>
-    <div class = "weight_edit">
-      <label for="price">体重</label>
-      <input type="text" name = "weight" value = "{{$infomation->weight}}">
-    </div>
-    <div>
-      <label for="name_edit">名前</label>
-      <input type="text" name = "name" value = "{{$infomation->name}}">
-    </div>
-    <button type = "submit">変更</button>
-    <a href="/infomation">戻る</a>
-  </form>
-</div>
-@section('title')
+@include('infomation/form',['target' => 'update'])
+@endsection
