00bd98385815026115628c08c058f3ab178285f6
hyurai <yuumanagai0504@icloud.com>
2021-02-27 13:57:55 +0900

    Create reqsample.blade.php

---
+++ my-laravel-app/resources/views/reqsample.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>サンプル</title>
+    </head>
+    <body>
+        <h1>クエリ文字</h1>
+        <p>nameパラメータ：{{$name}}</p>
+        <p>ageパラメータ：{{$age}}</p>
+    </body>
+</html>
