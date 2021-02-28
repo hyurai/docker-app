be83e5cdd425e58beafe7630d11d4ee4f31d4cff
hyurai <yuumanagai0504@icloud.com>
2021-02-28 22:17:59 +0900

    localscope_not

--- my-laravel-app/app/Infomation.php
+++ my-laravel-app/app/Infomation.php
@@ -3,12 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Infomation extends Model{
-
-    
-
     public function tweets(){
         return $this->hasmany(Tweet::class);
+    }
+
+    public function scopeTallHuman($query,$standardValue){
+        return $query->where('height','>',$standardValue);
+        
     }
 }
