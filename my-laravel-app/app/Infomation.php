d61c0c375f1cf70797a782b971022acb2733a43f
hyurai <yuumanagai0504@icloud.com>
2021-03-02 09:36:26 +0900

    GlobalScope_base

--- my-laravel-app/app/Infomation.php
+++ my-laravel-app/app/Infomation.php
@@ -6,13 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 
 class Infomation extends Model{
+    
     public function tweets(){
         return $this->hasmany(Tweet::class);
     }
 
     public function scopeTallHuman($query,$standardValue){
-        return $query->where('height','>',$standardValue);
+        return $query->where('height','>=',$standardValue);
         
+    }
+
+    protected static function boot(){
+        Infomation::boot();
+        static::addGlobalScope('height',function(Builder $builder){
+            $builder->where('height','>',100);
+        });
     }
 
 }
