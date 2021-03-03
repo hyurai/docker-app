4889e6f3fc24e71a0d2d31fdc30396a8ffc0ca56
hyurai <yuumanagai0504@icloud.com>
2021-03-03 14:27:18 +0900

    global&loccalscope_not_run

--- my-laravel-app/app/Infomation.php
+++ my-laravel-app/app/Infomation.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 
 class Infomation extends Model{
@@ -10,17 +11,18 @@ class Infomation extends Model{
     public function tweets(){
         return $this->hasmany(Tweet::class);
     }
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope('height',function(Builder $builder){
+            $builder->where('height','>',165);
+        });
+    }
 
     public function scopeTallHuman($query,$standardValue){
         return $query->where('height','>=',$standardValue);
         
     }
 
-    protected static function boot(){
-        Infomation::boot();
-        static::addGlobalScope('height',function(Builder $builder){
-            $builder->where('height','>',100);
-        });
-    }
+    
 
 }
