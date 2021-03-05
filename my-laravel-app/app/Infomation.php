99046ed8cf71a00a5a7e284d26aa20de8d76354e
hyurai <yuumanagai0504@icloud.com>
2021-03-05 15:27:46 +0900

    globalScope_base

--- my-laravel-app/app/Infomation.php
+++ my-laravel-app/app/Infomation.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Infomation extends Model
-{
+class Infomation extends Model{
+    
+    protected static function boot(){
+        parent::boot();
+        static::addGlobalScope('height',function(Builder $builder){
+            $builder->where('height','>',165);
+        });
+    }
     //
 }
+
